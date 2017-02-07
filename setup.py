cbd1b538613e68f0d30e3ad23708d264a22407f6
i-k-i <w5540554@gmail.com>
2017-02-07 15:39:15 +0300

    preparing package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='trev',
+    version='2017.02.07',
+    description = 'Show translation of selected text',
+    url='https://github.com/i-k-i/trev',
+    author='i-k-i project',
+    # author_email='',
+    license='WTFPL'
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), trev, 'README.md')).read(),
+    zip_safe=False
+)
+#🐍
