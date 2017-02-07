851fdec1c052df3c1d6b6f28507dc320b21984f6
i-k-i <w5540554@gmail.com>
2017-02-07 23:15:25 +0300

    pre_alpha

--- trev/def.config.py
+++ trev/def.config.py
@@ -3,11 +3,15 @@ import logging
 formatter = '[%(levelname)s]%(asctime)s - %(pathname)s:%(lineno)d - %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=formatter)
 
+# ***ll***
 # lingualeo auth
 # register
 ll_login = 'email'
 ll_passwd = 'passwd'
+# ***end ll***
 
+# ***ya***
 # yandex translate API key
 # get key:  https://tech.yandex.com/keys/get/?service=trnsl
 yatapi_key = 'yandex_api_key'
+# ***end ya***
